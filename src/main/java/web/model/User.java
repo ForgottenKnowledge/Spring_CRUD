e17cc5e60764d2afec7474b340f956ebfc61280f
Igor Anikeev <anikeev@softlab.ru>
2024-01-24 14:40:14 +0400

    Добавлен lombok

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,9 +1,16 @@
 package web.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @Column(name = "id")
@@ -16,45 +23,9 @@ public class User {
     @Column(name = "age")
     private Integer age;
 
-    public User() {
-    }
-
     public User(String name, String surname, Integer age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
 }
